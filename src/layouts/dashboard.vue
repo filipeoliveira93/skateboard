b34618c25b62035b84df83275590b4de80f67644
Filipe Oliveira <61127389+filipeoliveira93@users.noreply.github.com>
2022-11-09 22:38:45 -0300

    criação do menu

--- src/layouts/dashboard.vue
+++ src/layouts/dashboard.vue
@@ -1,16 +1,24 @@
 <template>
-	<div class="bg-gray-800 text-white">
-		<div>
-			<h3 class="font-semibold">Skateboard</h3>
-			<nav>
-				<h3>Menu</h3>
+	<div class="bg-gray-800 flex text-white max-h- h-screen">
+		<div class="border w-56">
+			<h3 class="font-semibold p-6 pt-3 block">Skateboard</h3>
+			<nav class="p-3">
+				<h1 class="opacity-50 uppercase font-semibold tracking-wider mb-6 text-xs">
+					Menu
+				</h1>
 				<div>
-					<div class="bg-orange-500">
-                        icon
-                    </div>
-                    <p>Discover </p>
+					<div class="space-y-6">
+						<menuitem/>
+					</div>
 				</div>
 			</nav>
 		</div>
+		<div class="border-2 flex-1 border-red-500">
+			<p>Content</p>
+		</div>
 	</div>
 </template>
+
+<script lang="ts" setup>
+	import menuitem from "/src/layouts/nav/menu_item.vue";
+</script>
---
+++ src/layouts/nav/menu_item.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex items-center space-x-4">
+        <div class="bg-orange-500 p-1.5  rounded-lg">
+            <homeicon class="w-4 v-4" />
+        </div>
+        <p>Discover</p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import homeicon from "/src/components/icons/homeicon.vue";
+</script>
