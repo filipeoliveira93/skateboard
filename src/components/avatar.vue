6e6e83e2f34b7f7f421fce90b75c0535de557f9d
Filipe Oliveira <filipeoliveira0426@gmail.com>
2022-11-20 20:52:11 -0300

    20/11

---
+++ src/components/avatar.vue
@@ -0,0 +1,3 @@
+<template>
+	<div class="flex"><img src="https://via.placeholder.com/200" class="rounded-full w-10 h10 " /></div>
+</template>
--- src/layouts/dashboard.vue
+++ src/layouts/dashboard.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="bg-gray-800 flex text-white max-h- h-screen">
 		<div class="w-56 drop-shadow-md">
-			<h3 class="font-semibold  p-6 pt-3 block">Skateboard</h3>
+			<h3 class="font-semibold p-6 pt-3 block">Skateboard</h3>
 			<nav class="p-3">
 				<h1 class="opacity-50 uppercase font-semibold tracking-wider mb-6 text-xs">
 					Menu
@@ -14,14 +14,24 @@
 				</div>
 			</nav>
 		</div>
-		<div class="flex-1 p-6">
-			<header>
-				<inputsearch />
-				<!-- <dropDown>
-					<avatar></avatar>
-				</dropDown>
-				<notifications/> -->
+		<div class="p-5 flex-1">
+			<header class="flex items-center justify-between border-solid">
+				<inputsearch class="w-96" />
+				<div class="space-x-3 flex items-center">
+					<avatar class=" rounded-full w-10 h-10" />
+					<span>Thomnas</span>
+					<downicon class="w-4 h-4" />
+					<bellicon class="w-4 h-4" />
+				</div>
+				<!-- <notifications /> -->
 			</header>
+			<h1 class="font-bold text-4xl mt-6 mb-4">Discover</h1>
+			<div class="grid grid-cols-12">
+				<div class="flex rounded-xl col-span-8 h-96 border border-green-500 overflow-hidden">
+					<img src="https://via.placeholder.com/800x400" alt="" class="w-full object-cover" />
+					<h3>How to do Basic Jumping and how to landing safely</h3>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -31,4 +41,7 @@ import menuitem from "/src/layouts/nav/menu_item.vue";
 import trendicon from "/src/components/icons/trendicon.vue";
 import homeicon from "/src/components/icons/homeicon.vue";
 import inputsearch from "/src/components/inputs/inputsearch.vue";
+import avatar from "/src/components/avatar.vue";
+import downicon from "/src/components/icons/down.vue";
+import bellicon from "/src/components/icons/bellicon.vue";
 </script>
